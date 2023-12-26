7724ce4948e1164991624ddde0ec7e7ff550f40d
Sin Nguyen <sinnguyen@Sins-MacBook-Air.local>
2023-12-26 00:25:43 -0800

    why can't I have an empty commit wtf?

--- src/app_paths.rs
+++ src/app_paths.rs
@@ -1,33 +1,46 @@
-use crate::{SharedState, AppError};
-use axum::{extract::{Json, State}, response::IntoResponse, http::StatusCode};
-use serde::Deserialize;
-use std::{sync::Arc, collections::hash_set};
+use crate::{AppError, SharedState};
+use axum::{
+    extract::{Json, State},
+    http::StatusCode,
+    response::IntoResponse,
+};
 use core::result::Result::Ok;
+use serde::{Deserialize, Serialize};
+use std::{collections::hash_set, sync::Arc};
+use mongodb::bson::doc;
+
+static DB_USER_LETTERS: &str = "user_letters";
+static COL_USER_TAGS: &str = "tags_per_user";
 
 // region: ↓ Save a Letter↓
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct SaveLetterPayload {
     email: String,
-    body: String,
-    tag_list: Vec<TagListPayload>,
+    letter: LetterDocument   
 }
-#[derive(Deserialize)]
-struct TagListPayload {
+#[derive(Deserialize, Serialize)]
+struct LetterDocument {
+    body: String,
+    tag_list: Vec<Tag>,
+}
+#[derive(Deserialize, Serialize)]
+struct Tag {
     //The ID is a composite name + color
-    id: String,
+    _id: String,
     name: String,
     color: String,
 }
-
 
 async fn save_letter(
     State(state): State<Arc<SharedState>>,
     Json(payload): Json<SaveLetterPayload>,
 ) -> Result<impl IntoResponse, AppError> {
-    if payload.body.len() > 10000 { return Ok(StatusCode::PAYLOAD_TOO_LARGE) };
+    if payload.letter.body.len() > 10000 {
+        return Ok(StatusCode::PAYLOAD_TOO_LARGE);
+    };
+    let client = &state.mongo_client;
+    let res = client.database(DB_USER_LETTERS).collection(&payload.email).insert_one(payload.letter ,None).await?;
     
- 
-
 
     Ok(())
 }
@@ -36,5 +49,3 @@ async fn save_letter(
 pub fn build(shared_state: Arc<SharedState>) -> Router {
     Router::new().with_state(shared_state)
 }
-
-
